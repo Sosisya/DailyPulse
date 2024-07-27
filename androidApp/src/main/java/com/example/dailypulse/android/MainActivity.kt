fcfb66a195c48db98b3311a4f4ab3dad594192fd
Sosisya <khaj-luizkin@yandex.ru>
2024-07-27 12:44:27 +0300

    Create Android articles screen

--- androidApp/src/main/java/com/example/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/dailypulse/android/MainActivity.kt
@@ -3,18 +3,17 @@ package com.example.dailypulse.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.dailypulse.Platform
+import com.example.dailypulse.android.screens.ArticlesScreen
+import com.example.dailypulse.articles.ArticlesViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        Platform().logSystemInfo()
+        val articlesViewModel: ArticlesViewModel by viewModels()
 
         setContent {
             MyApplicationTheme {
@@ -22,17 +21,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AboutScreen()
+                    ArticlesScreen(
+                        onAboutButtonClick = { showAboutDialog() },
+                        articlesViewModel = articlesViewModel
+                    )
                 }
             }
         }
     }
-}
 
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        AboutScreen()
-    }
+    private fun showAboutDialog() { }
 }
--- androidApp/src/main/java/com/example/dailypulse/android/screens/AboutScreen.kt
+++ androidApp/src/main/java/com/example/dailypulse/android/screens/AboutScreen.kt
@@ -1,8 +1,7 @@
-package com.example.dailypulse.android
+package com.example.dailypulse.android.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
