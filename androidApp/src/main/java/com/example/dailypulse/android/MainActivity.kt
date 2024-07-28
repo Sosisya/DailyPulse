eb7553e2e08acebdf22728c7bb35c937132142a2
Sosisya <khaj-luizkin@yandex.ru>
2024-07-28 12:29:35 +0300

    Create IOS articles screen. Add Navigation

--- androidApp/src/main/java/com/example/dailypulse/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/dailypulse/android/MainActivity.kt
@@ -6,14 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dailypulse.Platform
+import com.example.dailypulse.android.screens.AboutScreen
 import com.example.dailypulse.android.screens.ArticlesScreen
 import com.example.dailypulse.articles.ArticlesViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val articlesViewModel: ArticlesViewModel by viewModels()
+        val articlesViewModel : ArticlesViewModel by viewModels()
 
         setContent {
             MyApplicationTheme {
@@ -21,14 +25,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArticlesScreen(
-                        onAboutButtonClick = { showAboutDialog() },
-                        articlesViewModel = articlesViewModel
-                    )
+                    AppScaffold(articlesViewModel = articlesViewModel)
                 }
             }
         }
     }
-
-    private fun showAboutDialog() { }
 }
